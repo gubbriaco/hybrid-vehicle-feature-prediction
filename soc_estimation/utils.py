91d8fb3a1f8483b6128454d550add0be5a6971fb
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2024-08-21 10:12:43 +0200

    error functions

--- soc_estimation/utils.py
+++ soc_estimation/utils.py
@@ -9,3 +9,34 @@ def normalize(X):
         for j in range(X.shape[1]):
             normalized_X[i][j] = ((2*(X[i][j]-min_value))/(max_value-min_value))-1
     return normalized_X
+
+
+def rmse(observed, predicted):
+    diff = []
+    if len(predicted) != len(observed):
+        raise Exception(f'(len(predicted),len(observed))=({len(predicted)},{len(observed)}) => {len(predicted)}!={len(observed)}')
+    for i in range(len(predicted)):
+        diff_value = predicted[i] - observed[i]
+        quad_value = diff_value**2
+        diff.append(quad_value)
+    return np.sqrt(np.mean(diff))
+
+
+def maxv(observed, predicted):
+    diff = []
+    if len(predicted) != len(observed):
+        raise Exception(f'(len(predicted),len(observed))=({len(predicted)},{len(observed)}) => {len(predicted)}!={len(observed)}')
+    for i in range(len(predicted)):
+        diff_value = predicted[i] - observed[i]
+        diff.append(diff_value)
+    return np.max(np.abs(diff))
+
+
+def mae(observed, predicted):
+    diff = []
+    if len(predicted) != len(observed):
+        raise Exception(f'(len(predicted),len(observed))=({len(predicted)},{len(observed)}) => {len(predicted)}!={len(observed)}')
+    for i in range(len(predicted)):
+        diff_value = predicted[i] - observed[i]
+        diff.append(diff_value)
+    return np.mean(np.abs(diff))
